7d02860f6974c1a073551a630721a6fdc7c03f50
D3N2-A <anmolsharma999937@gmail.com>
2022-07-20 11:13:18 +0530

    Animation component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,36 @@
+import LogoTitle from "../../assets/images/logo-a.png";
+import { Link } from "react-router-dom";
+import "./index.scss";
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["n", "m", "o", "l"];
+  const wArray = [
+    "f",
+    "u",
+    "l",
+    "l",
+    "s",
+    "t",
+    "a",
+    "c",
+    "k",
+    "",
+    "w",
+    "e",
+    "b",
+    "",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
   return (
     <div className="container home-page">
       <div className="text-zone">
@@ -6,8 +38,15 @@ const Home = () => {
           Hi,
           <br />
           I'm
+          <img src={LogoTitle} alt="dev" />
+          <AnimatedLetters />
+          <br />
+          web developer
         </h1>
-        
+        <h2>Fullstack Web Developer.</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   );
