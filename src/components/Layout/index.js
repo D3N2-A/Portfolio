592065f0056a6d6665cedfa7336ce1cd2d86be1e
D3N2-A <anmolsharma999937@gmail.com>
2022-07-20 08:39:15 +0530

    Home component setup

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../Sidebar";
+import Sidebar from "../Sidebar/";
 import "./index.scss";
 
 const Layout = () => {
@@ -8,10 +8,13 @@ const Layout = () => {
       <Sidebar />
       <div className="page">
         <span className="tags top-tags">&lt;body&gt;</span>
+
         <Outlet />
-        <span className="tags bottom-tags">&lt;/body&gt;</span>
-        <br />
-        <span className="bottom-tag-html">&lt;/html&gt;</span>
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
       </div>
     </div>
   );
