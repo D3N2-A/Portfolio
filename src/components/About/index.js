f1cd0e8ee987debfac2d5c0ae1dcceb9c69daa86
D3N2-A <anmolsharma999937@gmail.com>
2022-07-22 08:37:50 +0530

    PreLoader added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,16 @@
 import "./index.scss";
 import AnimatedLettters from "../AnimatedLetters";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCss3,
+  faGit,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 const About = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
 
@@ -11,26 +21,52 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLettters
-            letterClass={letterClass}
-            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm ambitious front-end developer with some knowledge of backend. I'm
-          looking for oppurtunity to work on full stack projects with new
-          technology.
-        </p>
-        <p>
-          I'm quietly confident, naturally curious, and perpertually working on
-          improving my chops one design problem at a time.
-        </p>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLettters
+              letterClass={letterClass}
+              strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm ambitious front-end developer with some knowledge of backend.
+            I'm looking for oppurtunity to work on full stack projects with new
+            technology.
+          </p>
+          <p>
+            I'm quietly confident, naturally curious, and perpertually working
+            on improving my chops one design problem at a time.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#61DBFB" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGit} color="#F1502F" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faNodeJs} color="#68A063" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 export default About;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import "./index.scss";
 import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
 import Logo from "./Logo";
+import Loader from "react-loaders";
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
   const nameArray = ["n", "m", "o", "l"];
@@ -32,34 +33,37 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="dev" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={wArray}
-            idx={19}
-          />
-        </h1>
-        <h2>Fullstack Web Developer.</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="dev" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={wArray}
+              idx={19}
+            />
+          </h1>
+          <h2>Fullstack Web Developer.</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
