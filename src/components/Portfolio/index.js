eab43b2489c9f2750bf6918f0828ed2bd7badf2f
D3N2-A <anmolsharma999937@gmail.com>
2022-08-20 13:29:08 +0530

    project cards added

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,9 +4,49 @@ import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import portfolioData from "../../data/portfolio.json";
+import { useEffect } from "react";
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
-  console.log(portfolioData);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="images-box" key={idx}>
+              <img
+                src={port.cover}
+                alt={port.title}
+                style={{ width: "400px" }}
+                className="portfolio-image"
+              />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button
+                  className="btn"
+                  onClick={() => {
+                    window.open(port.url);
+                  }}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="container portfolio-page">
@@ -17,7 +57,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div>{renderPortfolio}</div>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="pacman" />
     </>
