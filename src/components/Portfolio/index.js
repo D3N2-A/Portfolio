e7fa793a4aea1a785b9e049a904dfa76ed097eb7
D3N2-A <anmolsharma999937@gmail.com>
2022-08-20 08:50:35 +0530

    portfolio data setup

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import { useState } from "react";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
-
+import portfolioData from "../../data/portfolio.json";
 const Portfolio = () => {
-  return <div>Hello</div>;
+  const [letterClass, setLetterClass] = useState("text-animate");
+  console.log(portfolioData);
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={"Portfolio".split("")}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
 };
 
 export default Portfolio;
